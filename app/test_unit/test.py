e8cc7f4d6023a02afbf9cb65e690e370198a6cd7
Антонов Алексей <AntonovAO@piteravto.ru>
2026-05-08 15:59:12 +0300

    fix tests

--- app/test_unit/test.py
+++ app/test_unit/test.py
@@ -1,5 +1,6 @@
 from app.core.proccesor import DocumentProcessor, VectorStore, RagManager
 import pytest
+import asyncio
 import os
 
 
@@ -14,15 +15,15 @@ def shared_store():
     return VectorStore(db_path="./test_db_tmp")
 
 @pytest.fixture()
-def store(shared_store):
-    shared_store.clear_db()
+async def store(shared_store):
+    await shared_store.clear_db()
     return shared_store
 
 @pytest.fixture()
-def rag():
+async def rag():
     
     manager = RagManager()
-    manager.store.clear_db()
+    await manager.store.clear_db()
     return manager
 
 class MockChunk:
@@ -30,33 +31,36 @@ class MockChunk:
         self.text = text
 
 
-
-def test_getDocument(shared_processor):
+@pytest.mark.asyncio
+async def test_getDocument(shared_processor):
     path = "data/СКУД/test.pdf"
     if os.path.exists(path):
-        chunks, file_name = shared_processor.getDocument(path)
+        chunks, file_name = await shared_processor.getDocument(path)
         assert chunks is not None
         assert len(chunks) > 0
     
-    assert shared_processor.getDocument('fake.pdf') is None
+    assert await shared_processor.getDocument('fake.pdf') is None
 
-def test_store_add(store):
-    initial_count = store.client.count(collection_name="my_docs").count
-    store.chunks2Collection([MockChunk("Тест заливки в БД")], "test.pdf")
-    new_count = store.client.count(collection_name="my_docs").count
+@pytest.mark.asyncio
+async def test_store_add(store):
+    initial_count = (await store.client.count(collection_name="my_docs")).count
+    await store.chunks2Collection([MockChunk("Тест заливки в БД")], "test.pdf")
+    new_count = (await store.client.count(collection_name="my_docs")).count
     assert new_count == initial_count + 1
 
-def test_upload_file(rag):
+@pytest.mark.asyncio
+async def test_upload_file(rag):
     path = "data/СКУД/test.pdf"
     if os.path.exists(path):
-        rag.upload_file(path)
-        count = rag.store.client.count(collection_name="my_docs").count
+        await rag.upload_file(path)
+        count = (await rag.store.client.count(collection_name="my_docs")).count
         assert count > 0
 
-def test_chunks2Collection(store):
+@pytest.mark.asyncio
+async def test_chunks2Collection(store):
 
     filename = "secret_manual.pdf"
-    store.chunks2Collection([MockChunk("Тест метаданных")], filename)
+    await store.chunks2Collection([MockChunk("Тест метаданных")], filename)
     
-    points, _ = store.client.scroll(collection_name="my_docs", limit=1, with_payload=True)
+    points, _ = await store.client.scroll(collection_name="my_docs", limit=1, with_payload=True)
     assert points[0].payload['metadatas'] == filename
